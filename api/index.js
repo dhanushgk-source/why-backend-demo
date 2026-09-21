21d89abcc9492246dfe2c03d86ce03d40e9e4bd8
techadmin-why <techadmin@thewhyservices.com>
2026-09-21 17:16:51 +0530

    Update api/index.js handler and vercel.json rewrites

--- api/index.js
+++ api/index.js
@@ -1,3 +1,8 @@
 const app = require("../src/app");
 
-module.exports = app;
+module.exports = (req, res) => {
+  if (req.url && !req.url.startsWith("/api") && req.url !== "/") {
+    req.url = "/api" + req.url;
+  }
+  return app(req, res);
+};
